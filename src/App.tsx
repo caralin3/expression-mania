60bdf8930597d3d3610ad4e8cf89cf8354e546f1
Cara Steinberg <caralin3@gmail.com>
2022-05-26 23:12:48 -0400

    chore: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,43 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store } from './store';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import { Counter, Logo } from './components';
+import { store } from './store';
+
+function Home() {
+  return (
+    <>
+      <main className="text-white">
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link className="text-white" to="/about">
+          About
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main className="text-white">
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link className="text-white" to="/">
+          Home
+        </Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   const persistor = persistStore(store);
@@ -19,7 +52,7 @@ function App() {
           }
         >
           <a
-            className="App-link text-white"
+            className="text-white"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -28,6 +61,10 @@ function App() {
           </a>
           <Logo size="lg" />
           <Counter />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
         </div>
       </PersistGate>
     </Provider>
