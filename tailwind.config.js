fbb372bb3773653e339ff73418b9cf8726712422
Cara Steinberg <caralin3@gmail.com>
2022-05-27 17:03:43 -0400

    chore: update css theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const { colors, fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media',
   theme: {
     extend: {
+      fontFamily: {
+        ...fontFamily,
+        sans: ['Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          ...colors.blue,
+          blizzard: '#a2eaf2',
+          cadet: '#3c737f',
+          columbia: '#c7e2eb',
+          dark: '#0e2f59',
+          prussian: '#142f40',
+          steel: '#587d8e',
+        },
+        green: {
+          ...colors.green,
+          sea: '#4a6a65',
+        },
+        yellow: {
+          ...colors.yellow,
+          pale: '#d7d9a7',
+        },
+      },
       spacing: {
         128: '32rem',
       },
