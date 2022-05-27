e8e86a120105b36aded72cf05c9cc16971d45999
Cara Steinberg <caralin3@gmail.com>
2022-05-26 21:14:24 -0400

    style: lint formatting

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './reducers';
 
